55e09a5fad48809cb5caa0eb466880a8a1396a1f
ReidarKristoffersen <kristoffersenreidar@gmail.com>
2024-11-18 12:16:12 +0100

    funksjonell Q
    Skal fungere, men gir bare rare verdier for x_s<0.3 ish. Disse er jo ikke veldig relevante da

--- Task10.py
+++ Task10.py
@@ -21,39 +21,52 @@ def P_2(x):
     return 1/2*(3*x**2-1)
 
 def y(x):
-    sum = 1
-    for n in range (100):
-        sum += ( (2*n*(2*n+1)+B_out/D) / (2*n+2)*(2*n+1) )**(n+1) * x**(2*n+2)
+    alpha_old = 1
+    alpha_new = 0
+    sum = 1 #The 1 represents alpha_0
+    for n in range (1000):
+        alpha_new = (2*n*(2*n+1)+B_out/D) / ((2*n+2)*(2*n+1)) *alpha_old
+        sum += alpha_new * x**(2*n+2)
+        alpha_old = alpha_new
     return sum
 
 def y_prime(x):
-    sum = 0
-    for n in range (100):
-        sum += ( (2*n*(2*n+1)+B_out/D) / (2*n+2)*(2*n+1) )**(n+1) * (2*n+2)*x**(2*n+1)
-    return y_prime
-
-def z(x):
-    #Må bli enig med viktor om utrykket
-    sum = 0
-    for n in range (100):
-        sum += 0
+    alpha_old = 1
+    alpha_new = 0
+    sum = 0 #Alpha_0 is no longer part of the sum due to derivation 
+    for n in range (1000):
+        alpha_new = (2*n*(2*n+1)+B_out/D) / ((2*n+2)*(2*n+1))*alpha_old
+        sum += (2*n+2)*alpha_new * x**(2*n+1)
+        alpha_old = alpha_new
     return sum
 
-def z_prime(x):
-    #Må bli enig med viktor om utrykket
-    sum = 0
-    for n in range (100):
-        sum += 0
+def z(s):
+    sum = 1 - B_out/(2*D) * s + (2+B_out/D)/8 * s**2
+    beta_old = (2+B_out/D)/8
+    beta_new = 0
+    for n in range (2, 1000):
+        beta_new = (n*(n+1) +B_out/D) / (2*(n+1)**2) * beta_old
+        sum += beta_new * s**(n+1)
+        beta_old = beta_new
     return sum
+
+def z_prime(s):
+    sum = - B_out/(2*D)  + (2+B_out/D)/8 * 2*s
+    beta_old = (2+B_out/D)/8
+    beta_new = 0
+    for n in range (2, 1000):
+        beta_new = (n*(n+1) +B_out/D) / (2*(n+1)**2) * beta_old
+        sum += beta_new * (n+1) * s**(n)
+        beta_old = beta_new
+    return -sum 
 
 def alpha_0_hat(x_s):
     gamma = ( (S_2*P_2(x_s)) / (6*D + B_out) + 1/B_out) * (a_u-a_l)
     lambd = (S_2*3*x_s / (6*D + B_out)) * (a_u-a_l)
-    return ( z_prime(x_s)*gamma - z(x_s)*lambd ) / ( y(x_s)*z_prime(x_s) - y_prime(x_s)*z_prime(x_s) )
+    return ( z_prime(1-x_s)*gamma - z(1-x_s)*lambd ) / ( y(x_s)*z_prime(1-x_s) - y_prime(x_s)*z_prime(1-x_s) )
 
 def Q(x_s):
-    #Legg inn Q
-    return (A_out + 273.15) / (B_out*(alpha_0_hat(x_s)*y(x_s) + a_l/B_out + S_2*a_l*P_2(x_s)/(6*D+B_out)))
+    return (A_out) / (B_out*(alpha_0_hat(x_s)*y(x_s) + a_l/B_out + S_2*a_l*P_2(x_s)/(6*D+B_out)))
 
 def a(x,x_s):
     a_arr = np.zeros(len(x))
